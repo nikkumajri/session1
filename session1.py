7190d904af097a16a9cc6a644d9fa19e56a4415e
nikkumajri <nikkumajri1998@gmail.com>
2024-05-27 18:11:47 +0930

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,36 @@
+# first program
+#calculate area of a circle
+# A = πr^2
+# area of circle = pie
+
+
+
+# write a program to inpute age of a user, and let them know if they have diabete:
+# risk or not. use case is that a user with age more than 65 is risky else not
+# risky
+age = 55
+if age>=65
+print{"this is risky"}
+else
+print{"not risky"}
+
+
+
+
+# write a program to calculate the GST tax of two products, apple or pie
+# apple has 10% GST and pie has only 8%
+# calculate total GST that need to be deducted from the bill
+# apple gross sales is $75, pie gross is $100
+
+
+
+apple = 75
+pie = 100
+gst_apple =0.1 apple
+gst_pie = 0.08 pie
+
+print{gst_apple}
+print{gst_pie}
+
+
+
